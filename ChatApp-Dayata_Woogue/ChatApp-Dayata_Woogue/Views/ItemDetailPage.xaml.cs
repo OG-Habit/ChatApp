dac939b50e5bb32e339364b34e18a8f069039f8c
OG-Habit <ivanwoogue2@gmail.com>
2022-04-13 15:43:50 +0800

    Add project files.

---
+++ ChatApp-Dayata_Woogue/ChatApp-Dayata_Woogue/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ChatApp_Dayata_Woogue.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ChatApp_Dayata_Woogue.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
